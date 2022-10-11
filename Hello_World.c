27d68334075c9896afe1e2d9c9e9b4034c43d9fa
Griaga <115525776+Griaga@users.noreply.github.com>
2022-10-11 17:38:37 +0800

    Update Hello_World.c

--- Hello_World.c
+++ Hello_World.c
@@ -2,14 +2,9 @@
 
 int main()
 {
-    int num1= 2022;
-    char letter='A'
-    ;float num2= 3.1416;
-
-    printf("value of num1 is %d/n",num1);
-    printf("value of letter is%c/n", letter);
-    printf("value of num2 is %f", num2);
-
+   
+    printf("HELLO WORLD");
+  
     return 0;
 
 }
