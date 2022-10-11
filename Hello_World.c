b88d14ab50ce995cbe61164d59f3f32e774ff6cd
Griaga <115525776+Griaga@users.noreply.github.com>
2022-10-11 17:34:51 +0800

    Add files via upload

---
+++ Hello_World.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1= 2022;
+    char letter='A'
+    ;float num2= 3.1416;
+
+    printf("value of num1 is %d/n",num1);
+    printf("value of letter is%c/n", letter);
+    printf("value of num2 is %f", num2);
+
+    return 0;
+
+}
+
+
+
